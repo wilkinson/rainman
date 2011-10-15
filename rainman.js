cceb02beff762bb3a0ef2f213fa18bb72f535b74
sean@mathbiol.org <sean@mathbiol.org>
2011-10-15 11:27:36 -0500

    Initial commit (no features!)

---
+++ rainman.js
@@ -0,0 +1,22 @@
+//- JavaScript source code
+
+//- rainman.js ~~
+//                                                      ~~ (c) SRW, 15 Oct 2011
+
+(function (global) {
+    'use strict';
+
+ // Declarations
+
+ // Definitions
+
+ // Invocations
+
+    return;
+
+}(function (outer) {
+    'use strict';
+    return (this === null) ? outer : this;
+}.call(null, this)));
+
+//- vim:set syntax=javascript:
